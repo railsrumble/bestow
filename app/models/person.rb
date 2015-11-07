bd6eabe2cf55604fc0248cbbb46bdb4a40eec793
Ashley Ellis <ashley@spreedly.com>
2015-11-07 10:02:35 -0500

    tests are awesome

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,7 +2,7 @@ class Person < ActiveRecord::Base
   belongs_to :user
 
   validates :name, presence: true
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/missing.png"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage/
 
 end
--- spec/controllers/people_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe PersonController, type: :controller do
-
-end
